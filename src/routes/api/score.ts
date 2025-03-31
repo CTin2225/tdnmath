81d76b714023ef4cae5f0233ad7ee2a21997c959
Endy3032 <endy3032@icloud.com>
2025-03-31 09:04:21 +0700

    debug api

--- src/routes/api/score.ts
+++ src/routes/api/score.ts
@@ -4,9 +4,11 @@ export const handler: Handlers = {
 	async POST(req: Request) {
 		console.log(req)
 		const data = await req.json()
+		console.log(data)
 		const kv = await Deno.openKv()
 		const { id, score, name } = data
 		const oldScore = await kv.get<{ score: number; name: string }>(["singlescores", id])
+		console.log(oldScore)
 		if (oldScore.value?.score && oldScore.value.score > score) {
 			await kv.set(["singlescores", id], { score, name })
 		}
