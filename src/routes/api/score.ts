bc6da7348ff1f30f76a10eeb63eb1e0633283c03
Endy3032 <endy3032@icloud.com>
2025-03-31 09:25:57 +0700

    fix scoring

--- src/routes/api/score.ts
+++ src/routes/api/score.ts
@@ -2,14 +2,11 @@ import { Handlers } from "$fresh/server.ts"
 
 export const handler: Handlers = {
 	async POST(req: Request) {
-		console.log(req)
 		const data = await req.json()
-		console.log(data)
 		const kv = await Deno.openKv()
 		const { id, score, name } = data
 		const oldScore = await kv.get<{ score: number; name: string }>(["singlescores", id])
-		console.log(oldScore)
-		if (oldScore.value?.score && oldScore.value.score < score) {
+		if (oldScore.value === null || oldScore.value.score && oldScore.value.score < score) {
 			await kv.set(["singlescores", id], { score, name })
 		}
 		return new Response("OK", { status: 200 })
